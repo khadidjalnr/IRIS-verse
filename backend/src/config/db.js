438a8cecf6372e01ee7a08da8b6b77359bf6a73a
KhadidjaLnr <khadidjalenouar2@gmail.com>
2026-03-10 13:20:18 +0100

    startServer func added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ import { ENV } from "./env.js";
 export const connectDB = async () => {
     try {
         await mongoose.connect(ENV.MONGO_URI);
-        console.log("Connected to DB successfully");
+        console.log("Connected to DB SUCCESSFULLY");
     } catch (error) {
         console.log("Error connecting to MONGODB");
         process.exit(1);
